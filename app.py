4de5d70a98cea4f02280d91fd5cc3ef682d7705c
Leonardo Torres <leoes98@gmail.com>
2022-02-10 14:28:36 -0700

    bug fixes, now with GCP API Docker Image

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ image = st.file_uploader("Choose an image", type=["jpeg", 'jpg', 'png'])
 if st.button("Make Prediction"):
     if image is not None:
         params = {"img_file": image.getvalue()}
-        api_url = "http://127.0.0.1:8000/predict"
-        res = requests.get(api_url, files=params)
+        api_url = "https://gcp-api-dr-cwcjlajpfq-uc.a.run.app/predict"
+        res = requests.post(api_url, files=params)
         pred = res.json()
         st.image(image, width=500)
         st.write(f"Class: {pred[-1][0]}")
